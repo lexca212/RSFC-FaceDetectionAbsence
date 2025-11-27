7423451064a75e5e72f218b74df557f86002ba4e
Henoch <henochyanuar13@gmail.com>
2025-11-27 22:22:42 +0700

    refactor(users): change urls users app

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,11 @@ from .views import *
 
 urlpatterns = [
     path('absence/', absence, name='absence'),
+
+    path('pengajuan_cuti/', pengajuan_cuti, name='pengajuan_cuti'),
+    path('edit_pengajuan_cuti/<int:id>', edit_pengajuan_cuti, name='edit_pengajuan_cuti'),
+    path('delete_pengajuan_cuti/<int:id>', delete_pengajuan_cuti, name='delete_pengajuan_cuti'),
+
+    path('jadwal/', jadwal, name='jadwal'),
+    path('presensi/', presensi, name='presensi')
 ]
--- app/views.py
+++ app/views.py
@@ -1,11 +1,15 @@
+from django.shortcuts import render, redirect, get_object_or_404
 from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile  
 from datetime import datetime, timedelta
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.contrib import messages
+from cms.decorators import login_auth
+from datetime import date, datetime
 from cms.models import *
 import face_recognition
 from .models import *
+import calendar
 import base64
 
 # Create your views here.
@@ -63,13 +67,21 @@ def absence(request):
                     ).first()
                     
                     if already_done:
-                        if already_done.schedule.name == 'Libur':
-                             default_storage.delete(temp_path) 
-                             return JsonResponse({ 'status': 'error', 'message': 'Tidak ada jadwal untuk hari ini. Libur... Boss' })
-                        
-                        default_storage.delete(temp_path) 
-                        return JsonResponse({ 'status': 'error', 'message': 'Anda kan sudah absen pulang hari ini...' })
+                        status = already_done.schedule.name
 
+                        messages = {
+                            'Libur': 'Tidak ada jadwal untuk hari ini. Libur... Boss',
+                            'Cuti': 'Tidak ada jadwal untuk hari ini. Katanya mau cuti !!'
+                        }
+
+                        message = messages.get(status, 'Anda kan sudah absen pulang hari ini...')
+
+                        default_storage.delete(temp_path)
+
+                        return JsonResponse({
+                            'status': 'error',
+                            'message': message
+                        })
                     # =====================================================
                     # CEK APAKAH SUDAH ADA ABSEN IN YANG BELUM OUT
                     # =====================================================
@@ -169,4 +181,193 @@ def absence(request):
         
     return render(request, 'user/absence.html')
 
+@login_auth
+def pengajuan_cuti(request):
+    user = get_object_or_404(Users, nik=request.session['nik_id'])
 
+    if request.method == 'POST':
+        leave_type_id = request.POST['cuti']
+        leave_type_obj = get_object_or_404(MasterLeaves, id=leave_type_id)
+        photo_file = request.FILES.get('photo') 
+        start_date = request.POST['start_date']
+        end_date = request.POST['end_date']
+        reason = request.POST['reason']
+
+        try:
+            new_leave_request = LeaveRequests(
+                nik=user,
+                leave_type=leave_type_obj,
+                start_date=start_date,
+                end_date=end_date,
+                reason=reason,
+            )
+            
+            if photo_file:
+                new_leave_request.photo = photo_file 
+            
+            new_leave_request.save()
+
+            messages.success(request, 'Data pengajuan cuti berhasil diupload.')
+            return redirect('pengajuan_cuti') 
+            
+        except Exception as e:
+            messages.error(request, f'Gagal mengupload data pengajuan cuti. Error: {e}')
+            return redirect('pengajuan_cuti')
+
+    cuti_list = MasterLeaves.objects.all()
+
+    pengajuan_list = LeaveRequests.objects.filter(
+        nik_id = user.nik
+    )
+            
+    context = {
+       'user': user,
+       'cuti_list':cuti_list,
+       'pengajuan_list': pengajuan_list,
+       'title': 'Pengajuan Cuti'
+    }
+
+    return render(request, 'user/cuti/index.html', context)
+
+@login_auth
+def edit_pengajuan_cuti(request, id):
+    user = get_object_or_404(Users, nik=request.session['nik_id'])
+
+    cuti_list = MasterLeaves.objects.all()
+
+    pengajuan = get_object_or_404(
+        LeaveRequests, 
+        nik_id=user.nik, 
+        id=id
+    )
+
+    if request.method == 'POST':
+        leave_type_id = request.POST['cuti']
+        leave_type_obj = get_object_or_404(MasterLeaves, id=leave_type_id)
+        photo_file = request.FILES.get('photo') 
+        start_date = request.POST['start_date']
+        end_date = request.POST['end_date']
+        reason = request.POST['reason']
+
+        try:
+            pengajuan = get_object_or_404(LeaveRequests, id=id)
+
+            pengajuan.leave_type =  leave_type_obj
+            pengajuan.start_date = start_date
+            pengajuan.end_date = end_date
+            pengajuan.reason = reason
+
+            if(photo_file):
+               pengajuan.photo = photo_file
+
+            pengajuan.save() 
+
+            messages.success(request, 'Data perubahan pengajuan cuti berhasil diupload.')
+            return redirect('pengajuan_cuti') 
+            
+        except Exception as e:
+            messages.error(request, f'Gagal mengupload perubahan data pengajuan cuti. Error: {e}')
+            return redirect('pengajuan_cuti')
+
+    context = {
+       'user': user,
+       'cuti_list':cuti_list,
+       'pengajuan': pengajuan,
+       'title': 'Edit Pengajuan Cuti'
+    }
+
+    return render(request, 'user/cuti/editForm.html', context)
+
+@login_auth
+def delete_pengajuan_cuti(request, id):
+    try:
+      cuti = get_object_or_404(LeaveRequests, id=id)
+      cuti.delete()
+
+      messages.success(request, 'Data pengajuan cuti berhasil dihapus.')
+      return redirect('/users/pengajuan_cuti')
+    
+    except Exception as e:
+      messages.error(request, f'Gagal menghapus data pengajuan cuti: {e}')
+      return redirect('/users/pengajuan_cuti')
+
+@login_auth
+def jadwal(request):
+    user = get_object_or_404(Users, nik=request.session['nik_id'])
+
+    today = date.today()
+    bulan = today.month
+    tahun = today.year
+
+    bulan_text = calendar.month_name[bulan]
+
+    total_hari = calendar.monthrange(tahun, bulan)[1]
+
+    jadwal_bulanan = []
+
+    data = MappingSchedules.objects.filter(
+        nik=user,
+        date__month=bulan,
+        date__year=tahun
+    ).select_related("schedule")
+
+    mapping = {
+        item.date.day: item.schedule.name 
+            if item.schedule.name in ["Libur", "Cuti"]
+            else item.schedule.start_time.strftime("%H:%M")
+        for item in data
+    }
+
+    for d in range(1, total_hari + 1):
+        current_date = date(tahun, bulan, d)
+        hari_text = calendar.day_name[current_date.weekday()]
+
+        jadwal_bulanan.append({
+            "tanggal": d,
+            "hari": hari_text,
+            "jadwal": mapping.get(d, "-")
+        })
+
+    context = {
+        "user": user,
+        "jadwal_bulanan": jadwal_bulanan,
+        "bulan_text": bulan_text,
+        "tahun": tahun,
+        'title': 'Jadwal Saya'
+    }
+
+    return render(request, "user/jadwal/index.html", context)
+
+def presensi(request):
+    user = get_object_or_404(Users, nik=request.session['nik_id'])
+
+    today = date.today()
+    bulan = today.month
+    tahun = today.year
+
+    absensis = InAbsences.objects.filter(
+        nik=user,
+        date_in__month=bulan,
+        date_in__year=tahun
+    ).order_by('-date_in')
+
+    absensi_bulanan = []
+    for abs in absensis:
+        absensi_bulanan.append({
+            "tanggal": abs.date_in.day,
+            "hari": abs.date_in.strftime("%A"),
+            "jam_in": abs.schedule.start_time.strftime("%H:%M") if abs.schedule.start_time else None,
+            "status_in": abs.status_in,
+            "jam_out": abs.schedule.end_time.strftime("%H:%M") if abs.schedule.end_time else None,
+            "status_out": abs.status_out,
+        })
+
+    context = {
+        'user': user,
+        "bulan_text": calendar.month_name[bulan],
+        "tahun": tahun,
+        "absensi_bulanan": absensi_bulanan,
+        "title": 'Riwayat absen saya '
+    }
+
+    return render(request, "user/absensi/index.html", context)
